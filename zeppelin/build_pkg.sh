2da7cf7b08e750aeac2d4961fcab10b12d6759da
johnomernik <john@omernik.com>
2016-09-07 09:20:54 -0500

    Initial rough commit of Apache Zeppelin

---
+++ zeppelin/build_pkg.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+CLUSTERNAME=$(ls /mapr)
+
+. /mapr/$CLUSTERNAME/zeta/kstore/env/zeta_shared.sh
+
+APP_NAME="zeppelin"
+
+
+APP_ROOT="/mapr/$CLUSTERNAME/zeta/shared/${APP_NAME}"
+APP_PKG_DIR="${APP_ROOT}/packages"
+
+
+mkdir -p $APP_PKG_DIR
+
+BUILD_TMP="./tmpbuilder"
+
+REQ_APP_IMG_NAME="maprbase buildbase"
+
+DOCKER_CHK=$(sudo docker ps)
+if [ "$DOCKER_CHK" == "" ]; then
+    echo "It doesn't appear your user has the ability to run Docker commands"
+    exit 1
+fi
+
+for IMG in $REQ_APP_IMG_NAME; do
+    RQ_IMG_CHK=$(sudo docker images|grep "\/${IMG}")
+    if [ "$RQ_IMG_CHK" == "" ]; then
+        echo "This install requires the the image $IMG"
+        echo "Please install this package before proceeding"
+        exit 1
+    fi
+done
+
+
+echo "To make your $APP package more complete, please specify a drill TGZ to include the JDBC jar for"
+echo ""
+
+DRILL_ROOT="/mapr/${CLUSTERNAME}/zeta/shared/drill"
+DRILL_PKG_DIR="$DRILL_ROOT/packages"
+
+ls -ls ${DRILL_PKG_DIR}
+echo ""
+read -e -p "Please enter the drill package to use for JDBC Drivers: " -i "drill-1.6.0.tgz" DRILL_TGZ
+DRILL_VER=$(echo -n ${DRILL_TGZ}|sed "s/\.tgz//")
+
+rm -rf ${BUILD_TMP}
+mkdir -p ${BUILD_TMP}
+cd ${BUILD_TMP}
+
+
+BUILD="Y"
+
+PCHECK=$(sudo docker images|grep zep_build)
+OCHECK=$(sudo docker images|grep zep_run)
+
+if [ "$PCHECK" == "" ] || [ "$OCHECK" == "" ]; then
+    echo "Building images because one of the two does not exist"
+else
+    read -e -p "Looks like the build and run images for Zeppelin already exist. Do you want to rebuild? " -i "N" BUILD
+fi
+
+APP_BUILD_IMG="${ZETA_DOCKER_REG_URL}/zep_build"
+APP_RUN_IMG="${ZETA_DOCKER_REG_URL}/zep_run"
+
+mkdir -p ./zep_build
+mkdir -p ./zep_run
+
+cat > ./zep_build/Dockerfile << EOF
+FROM ${ZETA_DOCKER_REG_URL}/buildbase
+
+RUN apt-get update && apt-get install -y openjdk-8-jdk git npm libfontconfig wget
+RUN wget http://apache.cs.utah.edu/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz && tar -zxf apache-maven-3.3.9-bin.tar.gz -C /usr/local/ && ln -s /usr/local/apache-maven-3.3.9/bin/mvn /usr/local/bin/mvn && rm apache-maven-3.3.9-bin.tar.gz && mkdir -p /app
+WORKDIR /app
+EOF
+
+cat > ./zep_run/Dockerfile << EOF1
+FROM ${ZETA_DOCKER_REG_URL}/maprbase
+RUN apt-get install -y python python-dev build-essential python-boto libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
+CMD ["python -V"]
+
+EOF1
+
+if [ "$BUILD" == "Y" ]; then
+    cd zep_build
+    sudo docker build -t $APP_BUILD_IMG .
+    cd ..
+    cd zep_run
+    sudo docker build -t $APP_RUN_IMG .
+    sudo docker push ${APP_RUN_IMG}
+    cd ..
+fi
+
+
+##############
+#Provide example GIT Settings
+APP_GIT_URL="https://github.com"
+APP_GIT_USER="apache"
+APP_GIT_REPO="incubator-zeppelin"
+git clone ${APP_GIT_URL}/${APP_GIT_USER}/${APP_GIT_REPO}
+
+
+echo "Create a mini build script"
+cat > ./${APP_GIT_REPO}/build.sh << EOF2
+#!/bin/bash
+export MAVEN_OPTS="-Xmx2g -XX:MaxPermSize=1024m"
+mvn clean package -DskipTests
+EOF2
+
+chmod +x ./${APP_GIT_REPO}/build.sh
+
+echo "Use the build image to build Zeppelin"
+
+sudo docker run -t --rm -v=`pwd`/${APP_GIT_REPO}:/app ${APP_BUILD_IMG} /app/build.sh
+
+
+
+echo "Getting Current version from pom.xml"
+cd ${APP_GIT_REPO}
+APP_VER=$(grep -m1 "<version>" pom.xml | cut -d">" -f2 | cut -d"<" -f1)
+cd ..
+
+echo "Pulling Drill JDBC Files"
+cp ${DRILL_PKG_DIR}/${DRILL_TGZ} ./
+tar zxf ${DRILL_TGZ}
+sudo cp ./${DRILL_VER}/jars/jdbc-driver/drill-jdbc-all* ./${APP_GIT_REPO}/interpreter/jdbc/
+rm -rf ./${DRILL_VER}
+
+
+echo "Packaging Zeppelin"
+sudo mv ${APP_GIT_REPO} "${APP_NAME}-${APP_VER}"
+APP_TGZ="${APP_NAME}-${APP_VER}.tgz"
+sudo chown -R zetaadm:zetaadm "${APP_NAME}-${APP_VER}"
+tar zcf ${APP_TGZ} ${APP_NAME}-${APP_VER}/
+
+##############
+# Finanlize location of pacakge
+
+# Tag and upload docker image if needed locally if needed (zeta is for local, but consider using the env variables for the roles)
+
+
+if [ -f "${APP_PKG_DIR}/${APP_TGZ}" ]; then
+    echo "This package already exists. We can exit now, without overwriting, or you can overwrite with the package you just built"
+    read -e -p "Should we overwrite ${APP_TGZ} located in ${APP_PKG_DIR} with the currently built package? (Y/N): " -i "N" OW
+    if [ "$OW" == "Y" ]; then
+        mv ${APP_TGZ} ${APP_PKG_DIR}/
+    else
+        echo "Not moving recently built package"
+    fi
+else
+    mv ${APP_TGZ} ${APP_PKG_DIR}/
+fi
+
+
+
+cd ..
+rm -rf ${BUILD_TMP}
+
+##############
+# Provide next step instuctions
+echo ""
+echo ""
+echo "${APP_NAME} release is prepped for use and uploaded to docker registry or copied to ${APP_PKG_DIR}"
+echo ""
+echo "Then, next step is to install a running instace of ${APP_NAME}"
+echo ""
+echo "> ${APP_ROOT}/install_instance.sh"
+echo ""
+echo ""
---
+++ zeppelin/install_instance.sh
@@ -0,0 +1,254 @@
+#!/bin/bash
+CLUSTERNAME=$(ls /mapr)
+
+. /mapr/$CLUSTERNAME/zeta/kstore/env/zeta_shared.sh
+
+APP_LIST_ALL="1"
+
+REC_DIR="zeta"
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+APP_NAME=$(basename "$MYDIR")
+
+. /mapr/$CLUSTERNAME/zeta/shared/preinst/install.inc.sh
+
+echo ""
+echo "List of current packages of $APP_NAME:"
+echo ""
+ls -ls $APP_PKG_DIR
+echo ""
+
+
+read -e -p "Which version of $APP_NAME do you want to use? " -i "zeppelin-0.7.0-SNAPSHOT.tgz" APP_BASE_FILE
+
+APP_BASE="${APP_PKG_DIR}/$APP_BASE_FILE"
+
+if [ ! -f "$APP_BASE" ]; then
+    echo "$APP_NAME Version specified doesn't exist, please download and try again"
+    echo "$APP_BASE"
+    exit 1
+fi
+APP_TGZ=$APP_BASE_FILE
+APP_VER=$(echo -n $APP_BASE_FILE|sed "s/\.tgz//")
+
+###############
+# $APP Specific
+echo "The next step will walk through instance defaults for ${APP_ID}"
+echo ""
+##########
+# Note: Template uses Docker Registery as example, you will want to change this
+# Get instance Specifc variables from user.
+read -e -p "Please enter the port for this instance of Zeppelin: " -i "43080" APP_PORT
+read -e -p "Please enter the total memory usage for this instance of ${APP_NAME}: " -i "2048" APP_TOTAL_MEM
+read -e -p "Please enter the memory usage for just $APP_NAME. This should be less then $APP_TOTAL_MEM: " -i "1024m" APP_MEM
+read -e -p "Please enter the CPU usage for this instance of ${APP_NAME}: " -i "2.0" APP_CPU
+read -e -p "Please enter the Username of the primary user of this instance of ${APP_NAME}: " -i "zetaadm" APP_USER
+
+
+
+##########
+# Do instance specific things: Create Dirs, copy start files, make executable etc
+cd ${APP_HOME}
+
+echo "Creating User Specific Directories"
+APP_USER_DIR="/mapr/${CLUSTERNAME}/user/${APP_USER}"
+APP_USER_ID_DIR="/mapr/${CLUSTERNAME}/user/${APP_USER}/${APP_NAME}/${APP_ID}"
+echo "Getting UID"
+
+APP_UID=$(id -u ${APP_USER})
+echo "UID: $APP_UID"
+
+if [ ! -d "${APP_USER_DIR}" ]; then
+    echo "The user provided, ${APP_USER}, does not appear to have a home directory:"
+    echo "${APP_USER_DIR}"
+    echo "Thus you can't install an instance here"
+    rm -rf ${APP_HOME}
+    exit 0
+fi
+if [ -d "${APP_USER_ID_DIR}" ]; then
+    echo "An instance directory for that user already exists"
+    echo "Will not overwrite, please choose a different name or remove the directory"
+    echo "${APP_USER_ID_DIR}"
+    rm -rf ${APP_HOME}
+    exit 0
+fi
+
+
+
+cp ${APP_ROOT}/start_instance.sh ${APP_HOME}/
+chmod +x ${APP_HOME}/start_instance.sh
+
+echo "Making Dirs"
+sudo mkdir -p ${APP_USER_ID_DIR}
+sudo mkdir -p ${APP_USER_ID_DIR}/notebooks
+sudo mkdir -p ${APP_USER_ID_DIR}/logs
+sudo mkdir -p ${APP_USER_ID_DIR}/conf
+echo "Untaring TGZ"
+sudo tar zxf ${APP_PKG_DIR}/${APP_TGZ} -C ${APP_USER_ID_DIR}
+echo "Setting up Conf"
+
+sudo cp ${APP_USER_ID_DIR}/${APP_VER}/conf/* ${APP_USER_ID_DIR}/conf/
+sudo cp ${APP_USER_ID_DIR}/conf/zeppelin-site.xml.template ${APP_USER_ID_DIR}/conf/zeppelin-site.xml
+
+sudo sed -i -r "s/<value>8080<\/value>/<value>${APP_PORT}<\/value>/" ${APP_USER_ID_DIR}/conf/zeppelin-site.xml
+sudo sed -i -r "s@<value>local-repo</value>@<value>/logs/local-repo</value>@" ${APP_USER_ID_DIR}/conf/zeppelin-site.xml
+
+sudo sed -i -r "s/org\.apache\.zeppelin\.spark\.PySparkInterpreter/org.apache.zeppelin.spark.PySparkInterpreter/" ${APP_USER_ID_DIR}/conf/zeppelin-site.xml
+
+sudo cp ${APP_USER_ID_DIR}/conf/zeppelin-env.sh.template ${APP_USER_ID_DIR}/conf/zeppelin-env.sh
+
+echo "Setting Permissions"
+sudo chown -R ${APP_USER}:zeta${APP_ROLE}data ${APP_USER_DIR}/zeppelin
+
+sudo cat > ${APP_USER_ID_DIR}/user_config.sh << EOU
+#!/bin/bash
+
+CLUSTERNAME=$(ls /mapr)
+MESOS_ROLE="${APP_ROLE}"
+APP_UP="${APP_UP}"
+APP_ID="${APP_ID}"
+
+. /mapr/\$CLUSTERNAME/zeta/kstore/env/zeta_${APP_ROLE}.sh
+
+RUSER=\$(whoami)
+
+
+
+
+echo "This script provides you the information to setup your interpreters in Zeppelin"
+echo ""
+echo "First Drill"
+echo "Based on roles, here are the options for Drill instances to connect to:"
+echo ""
+ls -ls /mapr/\$CLUSTERNAME/zeta/\${MESOS_ROLE}/drill/
+echo ""
+echo ""
+read -e -p "Please enter the name of the Drill Role to choose}: " -i "drill\${MESOS_ROLE}" DRILL_ID
+echo ""
+echo ""
+echo "on the Interpreters page, scroll to jdbc"
+echo ""
+echo "Here, set the following options and press save:"
+echo ""
+echo "default.user: \${RUSER}"
+echo "default.password: %ENTERYOURPASSWORD%"
+echo "default.url: jdbc:drill:zk=${ZETA_ZKS}/\%DRILL_ID%\/%DRILL_ID%"
+echo "default.driver: org.apache.drill.jdbc.Driver"
+echo "common.max_count: 10000"
+echo ""
+echo ""
+echo "While passwords show up in plain text in the web there is some security around them in the back ground"
+echo ""
+echo ""
+
+EOU
+sudo chmod +x ${APP_USER_ID_DIR}/user_config.sh
+sudo chown -R ${APP_USER}:zeta${APP_ROLE}data ${APP_USER_DIR}/zeppelin
+
+
+
+##########
+# Highly recommended to create instance specific information to an env file for your Mesos Role
+# Exampe ENV File for Docker Register V2 into sourced scripts
+
+cat > /mapr/$CLUSTERNAME/zeta/kstore/env/env_${APP_ROLE}/${APP_NAME}_${APP_ID}.sh << EOL1
+#!/bin/bash
+export ZETA_${APP_ID}_ID="${APP_ID}"
+export ZETA_${APP_ID}_PORT="${APP_PORT}"
+export ZETA_${APP_ID}_USER="${APP_USER}"
+export ZETA_${APP_ID}_URL="${APP_ID}.${APP_ROLE}.\${ZETA_MARATHON_ENV}.\${ZETA_MESOS_DOMAIN}:${APP_PORT}"
+EOL1
+
+##########
+# After it's written we source itSource the script!
+. /mapr/$CLUSTERNAME/zeta/kstore/env/env_${APP_ROLE}/${APP_NAME}_${APP_ID}.sh
+
+
+##########
+# Create a marathon file if appropriate in teh ${APP_HOME} directory
+# This actually updates the interpreter json so root or the owner can change
+cat > ${APP_HOME}/${APP_ID}.marathon << EOF
+{
+  "id": "${APP_ROLE}/${APP_ID}",
+  "cpus": ${APP_CPU},
+  "mem": ${APP_TOTAL_MEM},
+  "instances": 1,
+  "cmd":"su -c /zeppelin/bin/zeppelin.sh ${APP_USER}",
+  "labels": {
+   "PRODUCTION_READY":"True", "CONTAINERIZER":"Docker", "ZETAENV":"${APP_ROLE}"
+  },
+"env": {
+"ZEPPELIN_CONF_DIR":"/conf",
+"ZEPPELIN_NOTEBOOK_DIR":"/notebooks",
+"ZEPPELIN_HOME":"/zeppelin",
+"ZEPPELIN_LOG_DIR":"/logs",
+"ZEPPELIN_MEM":"-Xms${APP_MEM} -Xmx${APP_MEM} -XX:MaxPermSize=512m",
+"ZEPPELIN_PID_DIR":"/logs",
+"ZEPPELIN_INTERPRETER_LOCALREPO":"/logs/local-repo",
+"ZEPPELIN_DEP_LOCALREPO":"/logs/dep-repo",
+"MASTER":"mesos://leader.mesos:5050",
+"SPARK_HOME":"NotSet",
+"HADOOP_CONF_DIR":"NotSet",
+"ZEPPELIN_SPARK_CONCURRENTSQL":"true",
+"SPARK_APP_NAME":"zeppelinspark-$APP_ID",
+"DEBUG":"0"
+},
+  "container": {
+    "type": "DOCKER",
+    "docker": {
+      "image": "${ZETA_DOCKER_REG_URL}/zep_run",
+      "network": "HOST"
+    },
+  "volumes": [
+      {
+        "containerPath": "/zeppelin",
+        "hostPath": "${APP_USER_ID_DIR}/${APP_VER}",
+        "mode": "RO"
+      },
+      {
+        "containerPath": "/logs",
+        "hostPath": "${APP_USER_ID_DIR}/logs",
+        "mode": "RW"
+      },
+      {
+        "containerPath": "/notebooks",
+        "hostPath": "${APP_USER_ID_DIR}/notebooks",
+        "mode": "RW"
+      },
+      {
+        "containerPath": "/conf",
+        "hostPath": "${APP_USER_ID_DIR}/conf",
+        "mode": "RW"
+      }
+    ]
+  }
+}
+
+EOF
+
+
+#      {
+#        "containerPath": "/mapr/${CLUSTERNAME}",
+#        "hostPath": "/mapr/${CLUSTERNAME}",
+#        "mode": "RW"
+#      },
+
+
+###### WHERE I AM
+
+
+
+##########
+# Provide instructions for next steps
+echo ""
+echo ""
+echo "$APP instance ${APP_ID} installed at ${APP_HOME} and ready to go"
+echo "To start please run: "
+echo ""
+echo "> ${APP_HOME}/start_instance.sh"
+echo ""
+echo ""
+
+
+
